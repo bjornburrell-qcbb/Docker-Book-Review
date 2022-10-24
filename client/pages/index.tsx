6b770e11cbf50e0e909c934fb11bb37399990b72
Justin Tran <tranjg@mail.uc.edu>
2022-10-24 19:26:21 -0400

    feat: added client libraries (tailwindcss)

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,23 @@
 
 import { Button, Input } from 'antd'
 import type { NextPage } from 'next'
+import React, { useState } from 'react'
 
 
 const Home: NextPage = () => {
+  const [isbnInput, setISBNInput] = useState('');
+
   return (
-   <div className='flex'>
+   <div className='flex flex-col border-2'>
+    <div className='font-semibold'>
+      Enter ISBN Number:
+    </div>
+    <div className='flex flex-row'>
       <Input.Group compact>
       <Input style={{ width: 'calc(100% - 200px)' }}  />
       <Button type="primary">Submit</Button>
     </Input.Group>
+    </div>
    </div>
   )
 }
